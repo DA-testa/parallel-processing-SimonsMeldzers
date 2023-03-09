b905272c229e416ec0b498e07a8ed6734542ce57
SimonsMeldzers <56105687+SimonsMeldzers@users.noreply.github.com>
2023-03-09 16:21:51 +0000

    implemented input

--- main.py
+++ main.py
@@ -8,17 +8,12 @@ def parallel_processing(n, m, data):
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
